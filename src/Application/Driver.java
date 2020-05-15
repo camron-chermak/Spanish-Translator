fa71f0cc3926cdf7e1667b73344465a1e68e604c
Camron Chermak <camronchermak@Camrons-MBP.attlocal.net>
2020-05-14 18:24:25 -0700

    Fixed brackets and added Verb

--- src/Application/Driver.java
+++ src/Application/Driver.java
@@ -1,12 +1,48 @@
 package Application;
 
-public class Driver {
+import java.util.ArrayList;
 
-	public static void main(String[] args) {
-		
+public class Driver 
+{
+
+	public static void main(String[] args) 
+	{
+		// Example Noun
 		Word noun = new Noun("dog", "perro", Gender.MASCULINE);
 		System.out.println(noun);
-
+		
+		// Example Verb
+		Word word = new Verb("to dance", "bailar");
+		Verb verb = (Verb)word;
+		ArrayList<String> present = new ArrayList<String>();
+		present.add("bailo");
+		present.add("bailas");
+		present.add("baila");
+		present.add("bailamos");
+		present.add("bailan");
+		verb.setPresent(present);
+		ArrayList<String> preterit = new ArrayList<String>();
+		preterit.add("bailé");
+		preterit.add("bailaste");
+		preterit.add("bailó");
+		preterit.add("bailamos");
+		preterit.add("bailaron");
+		verb.setPreterit(preterit);
+		ArrayList<String> imperfect = new ArrayList<String>();
+		imperfect.add("bailaba");
+		imperfect.add("bailabas");
+		imperfect.add("bailaba");
+		imperfect.add("bailábamos");
+		imperfect.add("bailaban");
+		verb.setImperfect(imperfect);
+		ArrayList<String> future = new ArrayList<String>();
+		future.add("bailaré");
+		future.add("bailarás");
+		future.add("bailará");
+		future.add("bailaremos");
+		future.add("bailarán");
+		verb.setFuture(future);
+		System.out.println(verb.toString());
 	}
 
 }
---
+++ src/Application/Verb.java
@@ -0,0 +1,82 @@
+package Application;
+
+import java.util.ArrayList;
+
+public class Verb extends Word 
+{
+	private ArrayList<String> present;
+	private ArrayList<String> preterit;
+	private ArrayList<String> imperfect;
+	private ArrayList<String> future;
+	
+	public Verb(String english, String spanish) 
+	{
+		super(english, spanish);
+		this.present = new ArrayList<String>();
+		this.preterit = new ArrayList<String>();
+		this.imperfect = new ArrayList<String>();
+		this.future = new ArrayList<String>();
+	}
+	
+	public Verb(String english, String spanish, ArrayList<String> present, 
+			ArrayList<String> preterit, ArrayList<String> imperfect,
+			ArrayList<String> future) 
+	{
+		super(english, spanish);
+		this.present = present;
+		this.preterit = preterit;
+		this.imperfect = imperfect;
+		this.future = future;
+	}
+	
+	public void setPresent(ArrayList<String> present) 
+	{
+		this.present = present;
+	}
+	
+	public void setPreterit(ArrayList<String> preterit) 
+	{
+		this.preterit = preterit;
+	}
+	
+	public void setImperfect(ArrayList<String> imperfect) 
+	{
+		this.imperfect = imperfect;
+	}
+	
+	public void setFuture(ArrayList<String> future) 
+	{
+		this.future = future;
+	}
+	
+	public ArrayList<String> getPresent() 
+	{
+		return this.present;
+	}
+	
+	public ArrayList<String> getPreterit() 
+	{
+		return this.preterit;
+	}
+	
+	public ArrayList<String> getImperfect() 
+	{
+		return this.imperfect;
+	}
+	
+	public ArrayList<String> getFuture() 
+	{
+		return this.future;
+	}
+	
+	@Override
+	public String toString() {
+		String retString = "Verb{" + this.getSpanish() + " --> " + this.getEnglish() + "}"; 
+		retString += "\nPresent --> " + this.present.toString();
+		retString += "\nPreterit --> " + this.preterit.toString();
+		retString += "\nImperfect --> " + this.imperfect.toString();
+		retString += "\nFuture --> " + this.future.toString();
+		return retString;	
+	}
+	
+}
